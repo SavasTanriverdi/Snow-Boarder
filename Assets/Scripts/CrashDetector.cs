227377b4e7b5badc81234e810bd4ad8d11dacefa
Savaş Tanrıverdi <savas.tanriverdi@outlook.com>
2025-03-01 01:46:40 +0300

    Triggers To Restart Level

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Ground")
+        {
+            Debug.Log("Ouch, hit my head!");
+        }
+    }
+}
